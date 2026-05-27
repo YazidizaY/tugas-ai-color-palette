b6048da86af6e5c5950e38bb90b51514ad4e4ecd
Yazid Aqila Haikal <yazid24001@mail.unpad.ac.id>
2026-05-27 21:00:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sklearn.cluster import KMeans
 st.set_page_config(page_title="AI Color Palette Extractor", layout="centered")
 
 st.title("🎨 AI Color Palette Generator")
-st.write("Upload gambar kamu, dan AI (K-Means Clustering) akan mendeteksi 8 warna paling dominan secara otomatis.")
+st.write("Upload gambar kamu, dan AI (K-Means Clustering) akan mendeteksi 5 warna paling dominan secara otomatis.")
 
 uploaded_file = st.file_uploader("Pilih gambar (PNG, JPG, JPEG)...", type=["jpg", "jpeg", "png"])
 
@@ -21,15 +21,15 @@ if uploaded_file is not None:
     
     pixels = img_np.reshape(-1, 3)
     
-    kmeans = KMeans(n_clusters=8, random_state=42, n_init=10)
+    kmeans = KMeans(n_clusters=5, random_state=42, n_init=10)
     kmeans.fit(pixels)
     
     dominant_colors = kmeans.cluster_centers_.astype(int)
     
-    st.success("✨ Berhasil mengekstrak 8 warna dominan!")
+    st.success("✨ Berhasil mengekstrak 5 warna dominan!")
     
     st.subheader("Palet Warna Dominan:")
-    cols = st.columns(8)
+    cols = st.columns(5)
     
     for i, color in enumerate(dominant_colors):
         hex_color = '#{:02x}{:02x}{:02x}'.format(color[0], color[1], color[2])
